4d7d9818515e57e0d26fd6a87229c85cdf358a2b
madhavpreeth123 <111100703+madhavpreeth123@users.noreply.github.com>
2023-01-24 14:54:00 +0530

    Update aws.tf

--- aws.tf
+++ aws.tf
@@ -16,7 +16,7 @@ resource "aws_instance" "server" {
   }
 }
 resource "aws_key_pair" "key" {
-  key_name   = "sample"
+  key_name   = "sample2"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDT1CTmexrl5wbeqAN70zNZrNkThBYQE2R0kl9eJxry9D4vh2ffMbjTPeLRKq15vhlh3f2Q/NCFrdQgmT3/NC9AbKbiMuyKCxLTdVgTiRJH5Y/Zrg5DB10Znie09qN5pn3mNju9zHXhWJflCmjL8lJIeHQj+SYBkjwfsmTrYI2daWAw4DHKn7aUtp/xL3bsOcBsSUae7oCo7JNdQ2ij9S5d9wf0cAjL9fS2685UOOLkEAfxlGJI8P0w5vaqeCei99WPd46q0S1NMehHQHptpYd2Rv4NUTWe4rqk3sD2cdVpRLaf0yXNIgBORItG1Z1PwY86lwF8fHoboSXqaIlyNVG3Msa7drBR3g2J0qmKdyUBLngOHRa1R9RDMTxWP31n9+UO0OIZosLiRVTPqWOBXAzJrNNFqsqHGarW3I42Fof8jdpjyZF87e1+V63wS+o5rNtepi9i4tVOWpfhuMhoco+upAU6O4N04xD21xtq9XutjTwco+uup8OTeTb94BYiqis= madhav@APSS-LAP-33"
 
 }
